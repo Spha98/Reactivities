06e17d219630e04b176a54a354c80a4d940ee1cb
Ernest Siphamandla Mqcina <76960259+Spha98@users.noreply.github.com>
2022-07-26 13:56:44 +0200

    End of section 5

--- Application/Activities/List.cs
+++ Application/Activities/List.cs
@@ -17,12 +17,12 @@ namespace Application.Activities
         public class Handler : IRequestHandler<Query, List<Activity>>
         {
         private readonly DataContext _Context ;
-       
+
 
             public Handler(DataContext context)
             {
             _Context = context;
-           
+     
             }
 
             public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
